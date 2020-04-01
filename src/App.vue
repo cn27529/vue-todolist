daca8a2bc9edc1a656002de2b2ce1919fc05d0b8
cn27529 <cn27529@hotmail.com>
2020-04-01 22:02:36 +0800

    <li v-for="(todo, id) in todos" v-bind:key="id">

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
   <div id="app">
-    <Todos />
+    <h1>TodoList</h1>
+    <Todos v-bind:todos="todos" />
   </div>
 </template>
 
 <script>
-import Todos from './components/Todos';
+import Todos from './components/Todos'
 
 export default {
   name: 'App',
@@ -18,34 +19,41 @@ export default {
       todos: [
         {
           id: 1,
-          title: 'Todo One',
+          title: '台灣捐千萬片口罩助防疫 這些國家拿得到',
+          content: '',
           completed: false
         },
         {
           id: 2,
-          title: 'Todo Two',
+          title: '靠人民自律 瑞典「軟防疫」能堵疫情?',
+          content: '',
           completed: true
         },
         {
           id: 3,
-          title: 'Todo Three',
+          title: '2020/04/01 連假前夕抱股意願弱 台股失守9700點',
+          content: '',
           completed: false
         }
       ]
-    };
+    }
   }
-};
+}
 </script>
 
 <style>
 * {
   box-sizing: border-box;
-  margin: 0;
+  margin-top: 20;
   padding: 0;
 }
 
 body {
   font-family: Arial, Helvetica, sans-serif;
   line-height: 1.4;
+}
+
+h1 {
+  text-align: center;
 }
 </style>
--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,13 +1,27 @@
 <template>
   <div>
-    <h1>Todos</h1>
+    <ul>
+      <li v-for="(todo, id) in todos" v-bind:key="id">
+        <h3>
+          {{ todo.title }}
+        </h3>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Todos'
-};
+  name: 'Todos',
+  props: ['todos']
+}
 </script>
 
-<style scoped></style>
+<style scoped>
+h3 {
+  margin: 10px;
+}
+ul {
+  list-style-type: circle;
+}
+</style>

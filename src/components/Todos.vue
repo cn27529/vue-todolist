a0b8f451769b4deb9cd353995fe24e275542f3cc
cn27529 <cn27529@hotmail.com>
2020-04-01 18:12:04 +0800

    <Todos />

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
     <h1>Todos</h1>
+    <div for="todo in todos">
+        <h3>{{ todo.title }}</h3>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Todos'
+  name: 'Todos',
+  props: ['todos']
 };
 </script>
 
